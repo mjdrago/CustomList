9ad4f9a3772c36cc5afecc9f12c7585bab70f3c6
mjdrago <mario.j.drago@gmail.com>
2018-02-14 19:29:15 -0600

    Created C# console project. Added the CustomList class and a test project with references back to working solution project lives.

---
+++ CreateListClass/CreateListClassTest/CustomListTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CreateListClassTest;
+
+namespace CreateListClassTest
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

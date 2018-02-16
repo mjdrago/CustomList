ffebaa87f9bcc19b694639f755ad5830960426cc
mjdrago <mario.j.drago@gmail.com>
2018-02-15 20:56:17 -0600

    Created a method signature for the Add method and a few tests for it.

--- CreateListClass/CreateListClass/CustomList.cs
+++ CreateListClass/CreateListClass/CustomList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace CreateListClass
 {
-    class CustomList
+    public class CustomList<T>
     {
+        T[] activeArray;
+        public CustomList()
+        {
+            activeArray = new T[0];
+        }
+        public CustomList(int sizeOfInitialArray)
+        {
+            activeArray = new T[sizeOfInitialArray];
+        }
+
+        public T[] GetActiveArray()
+        {
+            return activeArray;
+        }
+        
+        public void Add(T elementToAdd)
+        {
+
+        }
     }
 }
--- CreateListClass/CreateListClassTest/CustomListTest.cs
+++ CreateListClass/CreateListClassTest/CustomListTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CreateListClassTest;
+using CreateListClass;
 
 namespace CreateListClassTest
 {
@@ -8,8 +8,34 @@ namespace CreateListClassTest
     public class CustomListTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddTest_AddAnInt5ToArray_Int5AtIndex0()
         {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>();
+            //Act
+            testCreation.Add(5);
+            //Assert
+            Assert.AreEqual(5, testCreation[0]);
+        }
+        [TestMethod]
+        public void AddTest_AddAnInt5ToArray_Count1()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>();
+            //Act
+            testCreation.Add(5);
+            //Assert
+            Assert.AreEqual(1, testCreation.Count);
+        }
+        [TestMethod]
+        public void AddTest_AddAnInt5ToArray_Capacity3()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>();
+            //Act
+            testCreation.Add(5);
+            //Assert
+            Assert.AreEqual(1, testCreation.Capacity);
         }
     }
 }

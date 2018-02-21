8c410968010cf5212960dce35baa4cb85624662b
mjdrago <mario.j.drago@gmail.com>
2018-02-20 19:33:27 -0600

    Wrote all tests for the add method. Create the backbone for the 3 type of constructors I'm planning, properties Count and Capacity, and an Indexer. Further, added the IEnumerable Interface to allow one of the constructors required.

--- CreateListClass/CreateListClass/CustomList.cs
+++ CreateListClass/CreateListClass/CustomList.cs
@@ -7,26 +7,61 @@ using System.Threading.Tasks;
 
 namespace CreateListClass
 {
-    public class CustomList<T>
+    public class CustomList<T>:IEnumerable<T>
     {
-        T[] activeArray;
+        T[] data;
         public CustomList()
         {
-            activeArray = new T[0];
+            data = new T[0];
         }
         public CustomList(int sizeOfInitialArray)
         {
-            activeArray = new T[sizeOfInitialArray];
+            data = new T[sizeOfInitialArray];
+        }
+        public CustomList(T[] initialArray)
+        {
+
         }
 
-        public T[] GetActiveArray()
+        public int Capacity
         {
-            return activeArray;
+            get
+            {
+                return 5;
+            }
+            set
+            {
+
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return 5;
+            }
+        }
+        public T this[int i]
+        {
+            get
+            {
+                return data[i];
+            }
         }
         
         public void Add(T elementToAdd)
         {
 
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- CreateListClass/CreateListClassTest/CustomListTest.cs
+++ CreateListClass/CreateListClassTest/CustomListTest.cs
@@ -8,7 +8,7 @@ namespace CreateListClassTest
     public class CustomListTest
     {
         [TestMethod]
-        public void AddTest_AddAnInt5ToArray_Int5AtIndex0()
+        public void AddTest_AddAnInt5ToDefaultArray_Int5AtIndex0()
         {
             //Arrange
             CustomList<int> testCreation = new CustomList<int>();
@@ -18,7 +18,7 @@ namespace CreateListClassTest
             Assert.AreEqual(5, testCreation[0]);
         }
         [TestMethod]
-        public void AddTest_AddAnInt5ToArray_Count1()
+        public void AddTest_AddAnInt5ToDefaultArray_Count1()
         {
             //Arrange
             CustomList<int> testCreation = new CustomList<int>();
@@ -28,14 +28,157 @@ namespace CreateListClassTest
             Assert.AreEqual(1, testCreation.Count);
         }
         [TestMethod]
-        public void AddTest_AddAnInt5ToArray_Capacity3()
+        public void AddTest_AddAnInt5ToDefaultArray_Capacity4()
         {
             //Arrange
             CustomList<int> testCreation = new CustomList<int>();
             //Act
             testCreation.Add(5);
             //Assert
-            Assert.AreEqual(1, testCreation.Capacity);
+            Assert.AreEqual(4, testCreation.Capacity);
+        }
+        [TestMethod]
+        public void AddTest_AddAStringHelloToDefaultArray_StringHelloAtIndex0()
+        {
+            //Arrange
+            CustomList<string> testCreation = new CustomList<string>();
+            //Act
+            testCreation.Add("Hello");
+            //Assert
+            Assert.AreEqual("Hello", testCreation[0]);
+        }
+        [TestMethod]
+        public void AddTest_AddAStringHelloToDefaultArray_Count1()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>();
+            //Act
+            testCreation.Add(5);
+            //Assert
+            Assert.AreEqual(1, testCreation.Count);
+        }
+        [TestMethod]
+        public void AddTest_AddAStringHelloToDefaultArray_Capacity4()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>();
+            //Act
+            testCreation.Add(5);
+            //Assert
+            Assert.AreEqual(4, testCreation.Capacity);
+        }
+
+        [TestMethod]
+        public void AddTest_Add3IntsToSizedArray_Count3()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(4);
+            //Act
+            testCreation.Add(4);
+            testCreation.Add(7);
+            testCreation.Add(1);
+            //Assert
+            Assert.AreEqual(3, testCreation.Count);
+        }
+        [TestMethod]
+        public void AddTest_AddMultiple3IntsToSizedArray_Capacity4()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(4);
+            //Act
+            testCreation.Add(4);
+            testCreation.Add(7);
+            testCreation.Add(1);
+            //Assert
+            Assert.AreEqual(4, testCreation.Capacity);
+        }
+        [TestMethod]
+        public void AddTest_AddMultiple3IntsToSizedArray_Int7AtIndex1()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(4);
+            //Act
+            testCreation.Add(4);
+            testCreation.Add(7);
+            testCreation.Add(1);
+            //Assert
+            Assert.AreEqual(7, testCreation[1]);
+        }
+        [TestMethod]
+        public void AddTest_AddMultiple3IntsToSizedArray_Int1AtIndex2()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(4);
+            //Act
+            testCreation.Add(4);
+            testCreation.Add(7);
+            testCreation.Add(1);
+            //Assert
+            Assert.AreEqual(1, testCreation[2]);
+        }
+        [TestMethod]
+        public void AddTest_AddMultiple4IntsToSizedArray_Capacity4()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(4);
+            //Act
+            testCreation.Add(4);
+            testCreation.Add(7);
+            testCreation.Add(1);
+            testCreation.Add(10);
+            //Assert
+            Assert.AreEqual(4, testCreation.Capacity);
+        }
+        [TestMethod]
+        public void AddTest_AddMultiple4IntsToSizedArray_Count4()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(4);
+            //Act
+            testCreation.Add(4);
+            testCreation.Add(7);
+            testCreation.Add(1);
+            testCreation.Add(10);
+            //Assert
+            Assert.AreEqual(4, testCreation.Count);
+        }
+        [TestMethod]
+        public void AddTest_AddMultiple4IntsToSizedArray_Capacity8()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(4);
+            //Act
+            testCreation.Add(4);
+            testCreation.Add(7);
+            testCreation.Add(1);
+            testCreation.Add(10);
+            testCreation.Add(3);
+            //Assert
+            Assert.AreEqual(8, testCreation.Capacity);
+        }
+        [TestMethod]
+        public void AddTest_AddMultiple4IntsToSizedArray_Count5()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(4);
+            //Act
+            testCreation.Add(4);
+            testCreation.Add(7);
+            testCreation.Add(1);
+            testCreation.Add(10);
+            testCreation.Add(3);
+            //Assert
+            Assert.AreEqual(5, testCreation.Count);
+        }
+        [TestMethod]
+        public void AddTest_AddValuesToAFilledArray_Int8AtIndex3()
+        {
+            //Arrange
+            CustomList<int> testCreation = new CustomList<int>(){5,4,3} ;
+            //Act
+            testCreation.Add(8);
+            //Assert
+            Assert.AreEqual(8, testCreation[3]);
         }
     }
 }
